9959e06c8c2a0d6e80971cbad4a5f15eb7d9bb1d
uchan-nos <uchan0@gmail.com>
2021-03-31 20:31:30 +0900

    supports UEFI header files

--- devenv/buildenv.sh
+++ devenv/buildenv.sh
@@ -1,5 +1,10 @@
 # Usage: source buildenv.sh
 
 BASEDIR="$HOME/osbook/devenv/x86_64-elf"
-export CPPFLAGS="-I$BASEDIR/include/c++/v1 -I$BASEDIR/include -I$BASEDIR/include/freetype2 -nostdlibinc -D__ELF__ -D_LDBL_EQ_DBL -D_GNU_SOURCE -D_POSIX_TIMERS"
+EDK2DIR="$HOME/edk2"
+export CPPFLAGS="\
+-I$BASEDIR/include/c++/v1 -I$BASEDIR/include -I$BASEDIR/include/freetype2 \
+-I$EDK2DIR/MdePkg/Include -I$EDK2DIR/MdePkg/Include/X64 \
+-nostdlibinc -D__ELF__ -D_LDBL_EQ_DBL -D_GNU_SOURCE -D_POSIX_TIMERS \
+-DEFIAPI='__attribute__((ms_abi))'"
 export LDFLAGS="-L$BASEDIR/lib"
